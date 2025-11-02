684058797aac44a89482a135e69d300f1e268f01
Archit Anand <archit_1901mm09@iitp.ac.in>
2025-11-02 11:21:23 +0530

    Create armstrong_numbers.cpp

---
+++ Basics/armstrong_numbers.cpp
@@ -0,0 +1,18 @@
+// https://www.geeksforgeeks.org/problems/armstrong-numbers2727/1
+// Difficulty: Easy
+
+// User function Template for C++
+class Solution {
+  public:
+    bool armstrongNumber(int n) {
+        // code here
+        int n2=n;
+        int sum=0;
+        while(n>0){
+            int d=n%10;
+            n/=10;
+            sum+=(d*d*d);
+        }
+        return sum==n2;
+    }
+};
