f38d0f20dd42c7870314f2c948d8c7bd221cc1cc
Archit Anand <archit_1901mm09@iitp.ac.in>
2025-11-02 11:18:11 +0530

    Create count_digits.cpp

---
+++ Basics/count_digits.cpp
@@ -0,0 +1,17 @@
+// https://www.geeksforgeeks.org/problems/count-digits-1606889545/1
+// Difficulty: Easy
+
+class Solution {
+  public:
+    int countDigits(int n) {
+        // code here
+        int c=0;
+        
+        while(n>0){
+            n/=10;
+            c++;
+        }
+        
+        return c;
+    }
+};
