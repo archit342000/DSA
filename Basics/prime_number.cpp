070b1c6d51e3cea74a0fb43c091c87ed6e739d4f
Archit Anand <archit_1901mm09@iitp.ac.in>
2025-11-02 11:25:20 +0530

    Create prime_number.cpp

---
+++ Basics/prime_number.cpp
@@ -0,0 +1,24 @@
+// https://www.geeksforgeeks.org/problems/prime-number2314/1
+// Difficulty: Easy
+
+class Solution {
+  public:
+    bool isPrime(int n) {
+        // code here
+        if(n==1){
+            return false;
+        }
+        if(n%2==0 && n!=2){
+            return false;
+        }
+        
+        
+        for(int i=2; i<n; i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
