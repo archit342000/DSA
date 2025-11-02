887a575aebb7f808ac9b61acd0565e3ed0384f26
Archit Anand <archit_1901mm09@iitp.ac.in>
2025-11-02 11:19:44 +0530

    Create gcd_of_two_numbers.cpp

---
+++ Basics/gcd_of_two_numbers.cpp
@@ -0,0 +1,74 @@
+// https://www.geeksforgeeks.org/problems/gcd-of-two-numbers3459/1
+// Difficulty: Easy
+
+class Solution {
+  public:
+    
+    int gcd(int a, int b) {
+        // code here
+        if(b>a){
+            gcd(b, a);
+        }
+        if(b==0){
+            return a;
+        }
+        return gcd(b, a%b);
+    }
+};
+
+
+class Solution {
+  public:
+    int gcd(int a, int b) {
+        // code here
+        while(a>0 && b>0){
+            if(a>b){
+                a=a%b;
+            }
+            else if(b>a){
+                b=b%a;
+            }
+            else{
+                break;
+            }
+        }
+        if(a==0){
+            return b;
+        }
+        else if(b==0){
+            return a;
+        }
+        else{
+            return a;
+        }
+    }
+};
+
+
+
+class Solution {
+  public:
+    int gcd(int a, int b) {
+        // code here
+        while(a>0 && b>0){
+            if(a>b){
+                a=a-b;
+            }
+            else if(b>a){
+                b=b-a;
+            }
+            else{
+                break;
+            }
+        }
+        if(a==0){
+            return b;
+        }
+        else if(b==0){
+            return a;
+        }
+        else{
+            return a;
+        }
+    }
+};
