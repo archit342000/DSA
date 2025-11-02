13b31eae6ff98cd94a34cc52a39b525cfc236f66
Archit Anand <archit_1901mm09@iitp.ac.in>
2025-11-02 11:30:35 +0530

    Create fibonacci_number.cpp

---
+++ Basics/fibonacci_number.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/fibonacci-number/description/
+// Difficulty: Easy
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0 || n==1){
+            return n;
+        }
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        for(int i=2; i<=n; i++){
+            c=a+b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+};
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0 || n==1){
+            return n;
+        }
+        return fib(n-1)+fib(n-2);
+    }
+};
