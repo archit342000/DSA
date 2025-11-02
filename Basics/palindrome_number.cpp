a31f45c323eb344c7a5cef8fe5a316ae03b39f61
Archit Anand <archit_1901mm09@iitp.ac.in>
2025-11-02 11:09:46 +0530

    Create palindrome_number.cpp

---
+++ Basics/palindrome_number.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/palindrome-number/description/
+// Difficulty: Easy
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        long long rev=0;
+        int x_2=x;
+
+        while(x_2!=0){
+            int d=x_2%10;
+            x_2/=10;
+            rev=rev*10+d;
+        }
+
+        return rev==x;
+    }
+};
