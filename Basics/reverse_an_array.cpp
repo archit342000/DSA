f425d156fd6dee0a96fc0276c097cb7657195256
Archit Anand <archit_1901mm09@iitp.ac.in>
2025-11-02 11:27:32 +0530

    Create reverse_an_array.cpp

---
+++ Basics/reverse_an_array.cpp
@@ -0,0 +1,34 @@
+// https://www.geeksforgeeks.org/problems/reverse-an-array/1
+// Difficulty: Easy
+
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        for(int i=0; i<n/2; i++){
+            arr[i]=arr[i]^arr[n-1-i];
+            arr[n-1-i]=arr[i]^arr[n-1-i];
+            arr[i]=arr[i]^arr[n-1-i];
+        }
+    }
+};
+
+
+class Solution {
+  public:
+    void reverseArrayHelper(vector<int> &arr, int n, int i){
+        if(i>=n/2){
+            return;
+        }
+        arr[i] = arr[i] ^ arr[n-1-i];
+        arr[n-1-i] = arr[i] ^ arr[n-1-i];
+        arr[i] = arr[i] ^ arr[n-1-i];
+        reverseArrayHelper(arr, n, i+1);
+    }
+    void reverseArray(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        reverseArrayHelper(arr, n, 0);
+    }
+};
